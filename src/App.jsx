5106a0f2b6f6f5d45938a0caa6fa1fb3ffca9c31
Talha <mdtalha2008@gmail.com>
2024-01-02 01:16:51 +0600

    ambientLight add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 import './App.css'
 import {Canvas} from "@react-three/fiber"
 
 
-const Cube = () => {
+const Cube = ({position,size,color}) => {
   return(
-    <mesh position={[1,0,0]}>
-        <boxGeometry/>
-        <meshStandardMaterial color={"orange"}/>
+    <mesh position={position}>
+        <boxGeometry args={size}/>
+        <meshStandardMaterial color={color}/>
       </mesh>
   )
 }
@@ -18,21 +19,14 @@ const App =()=> {
 
   return (
     <Canvas>
-      <directionalLight position={[0,0,2]}/>
-
-      <Cube/>
-      <mesh position={[-1,0,0]}>
-        <boxGeometry/>
-        <meshStandardMaterial color={"orange"}/>
-      </mesh>
-      <mesh position={[-1,2,0]}>
-        <boxGeometry/>
-        <meshStandardMaterial color={"orange"}/>
-      </mesh>
-      <mesh position={[1,2,0]}>
-        <boxGeometry/>
-        <meshStandardMaterial color={"orange"}/>
-      </mesh>
+      <directionalLight position={[0,0,2]} />
+      <ambientLight/>
+      <Cube position={[1,0,0]} color={"green"} size={[1,1,1]}/>
+      <Cube position={[-1,0,0]} color={"hotpink"} size={[1,1,1]}/>
+      <Cube position={[-1,2,0]} color={"blue"} size={[1,1,1]}/>
+      <Cube position={[1,2,0]} color={"yellow"} size={[1,1,1]}/>
+      
+      
     </Canvas>
   )
 }
