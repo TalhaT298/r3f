7b21020ed4b7e1dbe0888b904c343ab3cde03391
Talha <mdtalha2008@gmail.com>
2024-01-02 00:37:51 +0600

    another mesh add

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ const App =()=> {
   return (
     <Canvas>
       <directionalLight position={[0,0,2]}/>
+
       <mesh position={[1,0,0]}>
+        <boxGeometry/>
+        <meshStandardMaterial color={"orange"}/>
+      </mesh>
+      <mesh position={[-1,0,0]}>
         <boxGeometry/>
         <meshStandardMaterial color={"orange"}/>
       </mesh>
