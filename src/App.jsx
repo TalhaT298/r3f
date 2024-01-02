f3722ce08ee67ff5161f4ce35456dd1bff542492
Talha <mdtalha2008@gmail.com>
2024-01-02 12:44:08 +0600

    intensity add

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ const App =()=> {
 
   return (
     <Canvas>
-      <directionalLight position={[0,0,2]} />
-      <ambientLight/>
+      <directionalLight position={[0,0,2]} intensity={0.5}/>
+      <ambientLight intensity={0.1}/>
       <Cube position={[1,0,0]} color={"green"} size={[1,1,1]}/>
       <Cube position={[-1,0,0]} color={"hotpink"} size={[1,1,1]}/>
       <Cube position={[-1,2,0]} color={"blue"} size={[1,1,1]}/>
