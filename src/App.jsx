8ad73f12a795cb1f18232f2cedcbef1dd4b0fbf8
Talha <mdtalha2008@gmail.com>
2024-01-02 16:44:43 +0600

    install torusnot

--- src/App.jsx
+++ src/App.jsx
@@ -97,8 +97,8 @@ const App =()=> {
       </group> */}
       {/* <Cube position={[0,0,1]} size={[1,1,1]} color={"orange"}/> */}
       {/* <Sphere position={[0,0,0]} size={[1,30,30]} color={"orange"}/> */}
-      {/* <Torus position={[2,0,0]} size={[0.8,0.1,30,30]} color={"blue"}/>
-      <TorusKnot position={[-2,0,0]} size={[0.5,0.1,1000,50]} color={"hotpink"}/> */}
+      {/* <Torus position={[2,0,0]} size={[0.8,0.1,30,30]} color={"blue"}/> */}
+      <TorusKnot position={[-2,0,0]} size={[0.5,0.1,1000,50]} color={"hotpink"}/>
     </Canvas>
   )
 }
