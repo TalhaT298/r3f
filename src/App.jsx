032451ac9cf73d8296b7313cf8cc766a3243600b
Talha <mdtalha2008@gmail.com>
2024-01-02 13:21:20 +0600

    math position add

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ const Cube = ({position,size,color}) => {
   useFrame((state,delta) =>{
       ref.current.rotation.x += delta
       ref.current.rotation.y += delta*2.0
-      
+      ref.current.position.z=Math.sin(state.clock.elapsedTime)*2
+      console.
+
     })
   return(
     <mesh position={position} ref={ref}>
