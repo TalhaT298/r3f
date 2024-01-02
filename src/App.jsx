54916e7a8263e6f81ae4438abef5ced72cd47385
Talha <mdtalha2008@gmail.com>
2024-01-02 12:47:14 +0600

    group rotation

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,12 @@ const App =()=> {
     <Canvas>
       <directionalLight position={[0,0,2]} intensity={0.5}/>
       <ambientLight intensity={0.1}/>
+      <group position={[0,-1,0]}>
       <Cube position={[1,0,0]} color={"green"} size={[1,1,1]}/>
       <Cube position={[-1,0,0]} color={"hotpink"} size={[1,1,1]}/>
       <Cube position={[-1,2,0]} color={"blue"} size={[1,1,1]}/>
       <Cube position={[1,2,0]} color={"yellow"} size={[1,1,1]}/>
-      
+      </group>
       
     </Canvas>
   )
